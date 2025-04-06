6139472ef19fd811f83b6cc9b2ffb1cd8ee9021d
Hanish Tharwani <104623869+hanishtharwani123@users.noreply.github.com>
2025-04-06 19:29:39 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,10 +12,10 @@ const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <HeroSection />
-      <BioLinkSection />
-      <AIEngagementSection />
-      <SmartSchedulingSection />
-      <InfluenceWithoutLimitsSection />
+      <BioLinkSection lottieAnimationPath="/animations/Section_2_optimized.json" />
+      <AIEngagementSection lottieAnimationPath="/animations/Section_4_optimized.json" />
+      <SmartSchedulingSection lottieAnimationPath="/animations/Section_3_optimized.json" />
+      <InfluenceWithoutLimitsSection lottieAnimationPath="/animations/Section_5_optimized.json" />
       <PricingSection />
       <FAQSection />
       <Footer />
